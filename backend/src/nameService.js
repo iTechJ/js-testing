dc9c6d6fefbe89323b277eef2e4451d892345ef3
itechj <itechjavalab@gmail.com>
2017-09-08 00:12:38 +0300

    Create one more method for testing (getPersonFullName).

--- backend/src/nameService.js
+++ backend/src/nameService.js
@@ -2,6 +2,10 @@ module.exports = ((request) => {
     function getAmount(value, defaultValue) {
         return (typeof value === 'number' && value > 0) ? value : defaultValue;
     }
+    
+    function isNotBlankString(value) {
+        return typeof value === 'string' && value.trim() !== '';
+    }
 
     return {
         getRandomNames: (amount) => {
@@ -10,6 +14,14 @@ module.exports = ((request) => {
                 .then((rawData) => JSON.parse(rawData))
                 .then((data) => Array.isArray(data) ? data : [data])
                 .then((names) => names.map((nameObj) => nameObj.name));
+        },
+        getPersonFullName: (firstName, lastName, title) => {
+            if (isNotBlankString(firstName) && isNotBlankString(lastName)) {
+                const postfix = isNotBlankString(title) ? `, ${title}` : '';
+                return `${firstName} ${lastName}${postfix}`;
+            } else {
+                throw new Error('First and last name should be not blank.');
+            }
         }
     }
 })(require('request-promise-native'));
