a75c487901d55bbdda8b6d56dbab629debe3e1af
itechj <itechjavalab@gmail.com>
2017-09-08 00:14:42 +0300

    Create examples for chai library.

---
+++ backend/mocha-chai-synon/full-name.test.js
@@ -0,0 +1,67 @@
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+const should = chai.should(); //actually call the function
+
+//Additional chai configuration
+chai.config.includeStack = false;
+chai.config.showDiff = false;
+chai.config.truncateThreshold = 0; // disable truncating (default: 40)
+
+const nameService = require('../src/nameService');
+
+describe('Chai examples', () => {
+    const firstName = 'John';
+    const lastName = 'Doe';
+    const title = 'Ph.D.';
+    
+    const fullNameWithoutTitle = `${firstName} ${lastName}`;
+    const fullNameWithTitle = `${firstName} ${lastName}, ${title}`;
+
+    describe('different assertion styles', () => {
+        it('should return full name without comma when the title is not present', () => {
+            const result = nameService.getPersonFullName(firstName, lastName);
+
+            assert.equal(result, fullNameWithoutTitle, 'full name without title equal first and last name');
+            expect(result, '[full name without title]').to.equal(fullNameWithoutTitle);
+            result.should.equal(fullNameWithoutTitle);
+        });
+
+        it('should throw the error when last name is not present', () => {
+            const getResult = () => nameService.getPersonFullName(firstName);
+
+            assert.throws(getResult, 'First and last name should be not blank.');
+            expect(getResult).to.throw('First and last name should be not blank.');
+            should.Throw(getResult, 'First and last name should be not blank.')
+        });
+    });
+
+    describe('library toolkit', () => {
+        const childObj = {childProp1: 'abcd'};
+        const obj = {prop1: 'a', prop2: childObj};
+        const objCopy = Object.assign({}, obj);
+
+        it('objects comparison', () => {
+            expect(obj).to.equal(obj);
+            expect(obj).to.not.equal(objCopy);
+            expect(obj).to.deep.equal(objCopy);
+            expect(obj).to.deep.include({prop2: childObj});
+            expect(obj).to.have.deep.property('prop2', childObj);
+            expect(obj).to.nested.include({'prop2.childProp1': 'abcd'});
+        });
+
+        it('advanced methods chains', () => {
+            expect(obj)
+                .to.be.an('object')
+                .and.to.have.all.keys('prop1', 'prop2')
+                .but.not.to.have.any.keys('tmp1', 'tmp2');
+
+            expect([11, 3, obj])
+                .to.be.an('array')
+                .that.includes(3)
+                .with.deep.include(objCopy);
+        });
+    });
+});
+
+
